a1f1a0bd3d3126f35f99513a558c66546339390d
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-25 18:34:27 +0300

    //

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,16 @@
 from django.http import Http404
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 
 # Create your views here.
-async def post_list(request):
-    posts = await Post.published.all()
+def post_list(request):
+    posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
 
-async def post_detail(request, post_id):
-    try:
-        post = await Post.objects.aget(id=post_id)
-    except Post.DoesNotExist:
-        raise Http404("Post does not exist")
-    return render(request, 'blog/post/detail.html', {'post': post})
+def post_detail(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id, status=Post.Status.PUBLISHED)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
