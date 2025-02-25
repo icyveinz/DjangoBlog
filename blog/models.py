57dd8ce26adb638cf106df463c929b7398df33b9
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-25 15:15:46 +0300

    //

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager): # Кастомный менеджер написанный нами
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -22,6 +26,9 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+
+    objects = models.Manager()  # менеджер, применяемый по умолчанию
+    published = PublishedManager()  # конкретно-прикладной менеджер
 
     class Meta:
         ordering = ['-publish']
