1ef22e10b997ed527d1f1203e31705244d904956
Egor Ivanov <rust_lang_lover@proton.me>
2025-02-27 19:25:36 +0300

    //

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-
 from blog.models import Post
 
 
