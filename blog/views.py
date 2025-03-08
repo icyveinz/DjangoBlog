d8e6cfcba8ce6cf67ac075784c9c44b13a3161db
Egor Ivanov <rust_lang_lover@proton.me>
2025-03-08 13:28:03 +0300

    //

--- blog/views.py
+++ blog/views.py
@@ -35,11 +35,18 @@ def post_list(request):
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
                              slug=post,
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail-min.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
 
 @require_POST
 def post_comment(request, post_id):
