4691f1338789165d9c98f57ed58ff93f732c6777
Egor Ivanov <rust_lang_lover@proton.me>
2025-03-09 16:35:06 +0300

    //

--- blog/views.py
+++ blog/views.py
@@ -20,18 +20,31 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share-min.html', {'post': post, 'form': form})
 
 
-# Create your views here.
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
+    # Постраничная разбивка с 3 постами на страницу
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     try:
         posts = paginator.page(page_number)
     except PageNotAnInteger:
+        # Если page_number не целое число, то
+        # выдать первую страницу
         posts = paginator.page(1)
     except EmptyPage:
+        # Если page_number находится вне диапазона, то
+        # выдать последнюю страницу результатов
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list-min.html', {'posts': posts})
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts,
+                   'tag': tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
